bc2a0affbbd86bde2871aa572fc32d3322597e2e
Mohammed Saleh <34711999+MDSH14@users.noreply.github.com>
2021-07-29 01:18:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Mohammed Saleh',                   # Type in your name
   author_email = 'MohammedSaleh@ieee.org',      # Type in your E-Mail
   url = 'https://github.com/MDSH14',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/0.2.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[           # I get to this in a second
           'numpy',
