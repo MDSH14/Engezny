70da1247282f2150b272412610798d8b2ccd0c42
Mohammed Saleh <34711999+MDSH14@users.noreply.github.com>
2021-07-29 01:17:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Engezny',         # How you named your package folder (MyLib)
   packages = ['Engezny'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Quick generation for charts from DataFrame',   # Give a short description about your library
   author = 'Mohammed Saleh',                   # Type in your name
