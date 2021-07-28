e75b7dedf80f816aea181198fcd20fcabb6b9367
Mohammed Saleh <34711999+MDSH14@users.noreply.github.com>
2021-07-29 01:35:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Engezny',         # How you named your package folder (MyLib)
   packages = ['Engezny'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Quick generation for charts from DataFrame',   # Give a short description about your library
   author = 'Mohammed Saleh',                   # Type in your name
   author_email = 'MohammedSaleh@ieee.org',      # Type in your E-Mail
   url = 'https://github.com/MDSH14',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/0.3.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[           # I get to this in a second
           'numpy',
