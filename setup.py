22c26965ca2a47e9b21967244ec4f36f26b81a5a
Mohammed Saleh <34711999+MDSH14@users.noreply.github.com>
2021-07-29 01:53:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,21 @@ from distutils.core import setup
 setup(
   name = 'Engezny',         # How you named your package folder (MyLib)
   packages = ['Engezny'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Quick generation for charts from DataFrame',   # Give a short description about your library
   author = 'Mohammed Saleh',                   # Type in your name
   author_email = 'MohammedSaleh@ieee.org',      # Type in your E-Mail
   url = 'https://github.com/MDSH14',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/0.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/0.5.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[           # I get to this in a second
           'numpy',
           'pandas',
           'python-bidi',
           'arabic_reshaper',
-          'matplotlib'
+          'matplotlib',
+          'pillow'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
