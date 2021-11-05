72932f288d8478ca19392c0331ae325d331db73a
Mohammed Saleh <34711999+MDSH14@users.noreply.github.com>
2021-11-05 23:30:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author_email = 'MohammedSaleh@ieee.org',      # Type in your E-Mail
   url = 'https://github.com/MDSH14',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/1.0.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  keywords = ['Analysis', 'Data', 'Charts'],   # Keywords that define your package best
   install_requires=[           # I get to this in a second
           'numpy',
           'pandas',
