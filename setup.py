0dc1d43532febb7f940ada6d3f4ad56c921a8062
Mohammed Saleh <34711999+MDSH14@users.noreply.github.com>
2021-11-05 23:26:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'Engezny',         # How you named your package folder (MyLib)
   packages = ['Engezny'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Quick generation for charts from DataFrame',   # Give a short description about your library
   author = 'Mohammed Saleh',                   # Type in your name
   author_email = 'MohammedSaleh@ieee.org',      # Type in your E-Mail
   url = 'https://github.com/MDSH14',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/0.5.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/MDSH14/Engezny/archive/refs/tags/1.0.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[           # I get to this in a second
           'numpy',
@@ -19,7 +19,7 @@ setup(
           'pillow'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   # Again, pick a license
