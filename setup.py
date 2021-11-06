beddeef763c38774ba65c9911aa158353ff0aa33
Mohammed Saleh <34711999+MDSH14@users.noreply.github.com>
2021-11-06 02:34:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from distutils.core import setup
 from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = Path("README.md").read_text()
 setup(
   name = 'Engezny',         # How you named your package folder (MyLib)
   packages = ['Engezny'],   # Chose the same as "name"
